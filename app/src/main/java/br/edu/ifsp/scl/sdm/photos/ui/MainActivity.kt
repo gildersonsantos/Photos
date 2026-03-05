65334f6b4f960799f9d433b1ee526bcfddc0a620
Gilderson Santos <122750614+gildersonsantos@users.noreply.github.com>
2026-03-05 12:23:09 -0300

    feat: implementação da requisição de imagens e integração com RecyclerView.

--- app/src/main/java/br/edu/ifsp/scl/sdm/photos/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/photos/ui/MainActivity.kt
@@ -1,20 +1,28 @@
 package br.edu.ifsp.scl.sdm.photos.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.photos.R
 import br.edu.ifsp.scl.sdm.photos.adapter.ProductAdapter
+import br.edu.ifsp.scl.sdm.photos.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.photos.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.photos.model.Product
 import br.edu.ifsp.scl.sdm.photos.model.ProductList
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
+import java.io.BufferedInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.HttpURLConnection.HTTP_OK
 import java.net.URL
+import java.nio.Buffer
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
@@ -24,6 +32,11 @@ class MainActivity : AppCompatActivity() {
     private val productList: MutableList<Product> = mutableListOf()
     private val  productAdapter: ProductAdapter by lazy {
         ProductAdapter(this, productList)
+    }
+
+    private val productImageList: MutableList<Bitmap> = mutableListOf()
+    private val productImageAdapter: ProductImageAdapter by lazy {
+        ProductImageAdapter(this, productImageList)
     }
 
     companion object {
@@ -38,7 +51,26 @@ class MainActivity : AppCompatActivity() {
             title = getString(R.string.app_name)
         })
 
-        amb.productsSp.adapter = productAdapter
+        amb.productsSp.apply {
+            adapter = productAdapter
+            onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val size = productImageList.size
+                    productImageList.clear()
+                    productImageAdapter.notifyItemRangeRemoved(0, size)
+                    retrieveProductImages(productList[position])
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
+
+        amb.productImagesRv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = productImageAdapter
+        }
         retrieveProducts()
     }
 
@@ -58,6 +90,29 @@ class MainActivity : AppCompatActivity() {
             runOnUiThread { Toast.makeText(this, getString(R.string.response_problem), Toast.LENGTH_SHORT).show() }
         } finally {
             productsConnection.disconnect()
+        }
+    }.start()
+
+    private fun retrieveProductImages(product: Product) = Thread{
+        product.images.forEach { imageUrl ->
+            val imageConnection =URL(imageUrl).openConnection() as HttpURLConnection
+            try {
+                if (imageConnection.responseCode == HTTP_OK) {
+                    BufferedInputStream(imageConnection.inputStream).let {
+                        val imageBitmap = BitmapFactory.decodeStream(it)
+                        runOnUiThread {
+                            productImageList.add(imageBitmap)
+                            productImageAdapter.notifyItemInserted(productImageList.lastIndex)
+                        }
+                    }
+                } else {
+                    runOnUiThread { Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show() }
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread { Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show() }
+            } finally {
+                imageConnection.disconnect()
+            }
         }
     }.start()
 }
